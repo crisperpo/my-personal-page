1b79bd2f49fd12ba28913fa6d9e473decba0e0d0
crisperpo <crispe1586@gmail.com>
2025-03-27 18:34:24 +0100

    clean up scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/app.css';
+import content from './lib/constants/content.json';
+
+const App = () => {
+  return (
+    <>
+      {content.title}
+    </>
+  )
+};
+
+export default App;
