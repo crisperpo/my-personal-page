42b8ff2e0cebd971f61a3877f5e54719e37ee972
crisperpo <crispe1586@gmail.com>
2025-03-27 20:29:10 +0100

    add basic UI

---
+++ src/components/App/index.styled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+    align-items: center;  
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    justify-content: center;
+`;
+
+export {
+    HeaderContainer
+};
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+import content from '../lib/constants/content.json';
+
+const Title = () => <div>{content.title}</div>;
+
+export default Title;
