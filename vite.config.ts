94492c60bc74a05049f82bd4b118f9e8f5a4a746
crisperpo <crispe1586@gmail.com>
2025-03-28 23:21:20 +0100

    remove paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-//@ts-ignore
-import { AliasOptions, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react()]
 })
