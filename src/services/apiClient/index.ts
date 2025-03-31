4db421ec7ef02998089407c948f42050c89b3ad0
crisperpo <crispe1586@gmail.com>
2025-03-31 23:36:47 +0200

    get baseURL from env

--- src/services/apiClient/index.ts
+++ src/services/apiClient/index.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
-  timeout: 5000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default apiClient;
