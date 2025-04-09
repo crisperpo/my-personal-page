b0f622a45f1e819eee03c46c418fc0e38cd14ba5
crisperpo <crispe1586@gmail.com>
2025-04-09 14:44:15 +0200

    add and configure vite and testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest/config" />
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,6 +9,10 @@ export default defineConfig({
   base: './',
   plugins: [react()],
   test: {
-    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)']
+    css: true,
+    environment: 'jsdom',
+    globals: true,
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    setupFiles: './src/test/setup.ts',
   },
 })
