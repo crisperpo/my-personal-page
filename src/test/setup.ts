38af8516a0182f104ed33473e4926acec30a272b
crisperpo <crispe1586@gmail.com>
2025-04-09 18:10:18 +0200

    add mock contenful data

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,20 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+
+import mockContentfulData from './mockContentfulData';
+
+const { headline, socialNetworks, title } = mockContentfulData;
+
+vi.mock('react', async () => {
+    const actualReact = await vi.importActual('react');
+    return {
+        ...actualReact,
+        useContext : vi.fn(() => ({
+            pageData: {
+                headline: [headline[0], headline[1], headline[2]],
+                title,
+                socialNetworks,
+            }
+        }))
+    };
+});
