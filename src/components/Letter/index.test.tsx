f66e1c6ae1e3fd77d40b63eeb6962ea99a93acc8
crisperpo <crispe1586@gmail.com>
2025-04-09 20:09:36 +0200

    add tests letter

---
+++ src/components/Letter/index.test.tsx
@@ -0,0 +1,53 @@
+import Letter from './index';
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import colors from '../../lib/constants/colors';
+
+const MOCK_LETTER = 'A';
+
+describe('Letter Component', () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+    it('should render correctly', () => {
+        render(
+            <Letter letter={MOCK_LETTER} position={0} />
+        );
+    });
+    it('should contain the class "letter"', () => {
+        render(
+            <Letter letter={MOCK_LETTER} position={0} />
+        );
+        const letterElement = screen.getByText(MOCK_LETTER).closest('div');
+        expect(letterElement).toHaveClass('letter');
+    });
+    it('should contain the class "hovered" when isHovered is true', () => {
+        vi.mock('react', async () => {
+            const actualReact = await vi.importActual('react');
+            return {
+                ...actualReact,
+                useState: vi.fn().mockImplementation(() => [true, vi.fn()]),
+            };
+        });
+
+        render(<Letter letter={MOCK_LETTER} position={0} />);
+        const letterElement = screen.getByText(MOCK_LETTER).closest('div');
+        expect(letterElement).toHaveClass('hovered');
+    });
+    it('should render a div containing the letter with the correct color', () => {
+        const MOCK_POSITION = 1;
+        const colorKeys = Object.values(colors.title);
+        const color = colorKeys[MOCK_POSITION % colorKeys.length];
+        render(
+            <Letter letter={MOCK_LETTER} position={MOCK_POSITION} />
+        );
+        const letterDiv = screen.getByText(MOCK_LETTER).closest('div');
+        expect(letterDiv).toHaveStyle(`color: ${color}`);
+    });
+    it('matches the snapshot', () => {
+        const { asFragment } = render(
+            <Letter letter={MOCK_LETTER} position={0} />
+        );
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
