7a35e3d261c07d1028a89308660045a172e41e87
Cristina Restrepo <crispe1586@gmail.com>
2025-05-09 21:38:49 +0200

    add Headline container

---
+++ client/src/components/Headline/index.styled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import type { ThemeType } from "../../lib/types/types.d";
+
+const HeadlineContainer = styled.span<{ theme: ThemeType }>``;
+
+export {
+    HeadlineContainer
+};
