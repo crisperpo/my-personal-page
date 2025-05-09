6ef961658621440048aa50dfca75acd42ec056b7
Cristina Restrepo <crispe1586@gmail.com>
2025-05-10 01:41:30 +0200

    add compression

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest/config" />
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { compression } from 'vite-plugin-compression2';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), compression()],
   test: {
     css: true,
     environment: 'jsdom',
