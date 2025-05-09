80f655903351632965d643d953e75861d2be2372
Cristina Restrepo <crispe1586@gmail.com>
2025-05-09 23:00:33 +0200

    remove mode

--- client/src/context/AppProvider.tsx
+++ client/src/context/AppProvider.tsx
@@ -9,11 +9,10 @@ import type { PageDataType } from '../lib/types/types.d';
 export const AppProvider = ({ children }: { children: ReactNode }) => {
     const isDarkMode = useDetectDarkModeAndReload();
     const [pageData, setPageData] = useState<PageDataType | null>(null);
-    const mode = isDarkMode ? 'dark' : 'light';
     const theme = isDarkMode ? themes.dark : themes.light;
 
     return (
-        <AppContext.Provider value={{ mode, pageData, setPageData, theme }}>
+        <AppContext.Provider value={{ pageData, setPageData, theme }}>
             {children}
         </AppContext.Provider>
     );
