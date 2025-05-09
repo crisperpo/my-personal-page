9b944bba42bfec7ca7a58a9edf63805de3ab76d0
Cristina Restrepo <crispe1586@gmail.com>
2025-05-09 23:22:33 +0200

    use useTheme from styled components

--- client/src/pages/App/index.tsx
+++ client/src/pages/App/index.tsx
@@ -7,14 +7,16 @@ import Headline from '../../components/Headline/';
 import Social from '../../components/Social/';
 import { HeaderContainer } from './index.styled';
 import { AppContext } from '../../context/AppContext';
-import useTheme from '../../hooks/useTheme';
+import useDetectDarkModeAndReload from '../../hooks/useDetectDarkModeAndReload';
 import '../../styles/app.css';
+import theme from '../../lib/constants/theme';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const appContext = useContext(AppContext);
+  const isDarkMode = useDetectDarkModeAndReload();
   const { setPageData } = appContext || {};  
-  const currentTheme = useTheme();
+  const currentTheme = isDarkMode ? theme.dark : theme.light;
 
   useEffect(() => {
     const fetchData = async () => {
