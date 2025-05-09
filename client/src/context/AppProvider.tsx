8297b80461b6bec230e3dee2026937a95621951e
Cristina Restrepo <crispe1586@gmail.com>
2025-05-09 21:44:43 +0200

    Move theme provider to main

--- client/src/context/AppProvider.tsx
+++ client/src/context/AppProvider.tsx
@@ -1,18 +1,15 @@
 import { useState, ReactNode } from 'react';
-import { ThemeProvider } from 'styled-components';
 
 import { AppContext } from './AppContext';
-import theme from '../lib/constants/theme';
+
 import type { PageDataType } from '../lib/types/types.d';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
     const [pageData, setPageData] = useState<PageDataType | null>(null);
 
     return (
-        <ThemeProvider theme={theme}>
-            <AppContext.Provider value={{ pageData, setPageData }}>
-                {children}
-            </AppContext.Provider>
-        </ThemeProvider>
+        <AppContext.Provider value={{ pageData, setPageData }}>
+            {children}
+        </AppContext.Provider>
     );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import App from './pages/App/';
 import { AppProvider } from './context/AppProvider';
+import theme from '../src/lib/constants/theme';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </AppProvider>
   </StrictMode>,
 );
